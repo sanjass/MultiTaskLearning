73174e89d35011ad20ed19699dcb0efbdca574f3
Sanja Simonovikj <sanjas@mit.edu>
2020-11-24 01:48:45 -0500

    fixes to data parsing ex2

--- ex2_data_parsing/data_parse.py
+++ ex2_data_parsing/data_parse.py
@@ -1,18 +1,26 @@
 import csv
 import os
 import json
+import re
 
 def check_numeric(question):
     for i in range(1, 5):
-        if not question[i].isnumeric():
+        if not question[i].replace(",","").isnumeric():
             return False
     return True
 
 def gen_quant_cells(question):
     lst = []
     for i in range(len(question[0].split(" "))):
-        lst += [i+1]
+        lst += [i]
     return lst
+
+def check_contains_num(question):
+    pattern = re.compile('\d+,\d+|\d*\.\d+|\d+')
+    for token in question[0].split(' '):
+        if pattern.fullmatch(token):
+           return True
+    return False
 
 def gen_answer(question):
     answer = question[5]
@@ -34,7 +42,7 @@ for file in os.listdir(directory):
     with open(directory+file, newline='') as csvfile:
         reader = csv.reader(csvfile, delimiter=',', quotechar='"')
         for row in reader:
-            if check_numeric(row):
+            if check_numeric(row) and check_contains_num(row):
                 question_dict = {"quant_cell_positions":gen_quant_cells(row),
                                 "processed_question": row[0],
                                 "raw_question": row[0],
@@ -44,7 +52,7 @@ for file in os.listdir(directory):
                 answer_dict = {"answer": gen_answer(row), "Id": id}
                 answers.append(answer_dict)
                 id += 1
-
+print(len(questions))
 
 with open('test.json', 'w') as outfile:
     json.dump(questions, outfile)
